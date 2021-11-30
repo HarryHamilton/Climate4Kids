366a5a20fd6ba96966062283fb0d22bc2a847ac9
JamesNewsome02 <J.Newsome2@newcastle.ac.uk>
2021-11-30 13:31:00 +0000

    Created Files

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,12 +1,5 @@
-from flask import Flask
+from flaskr.__init__ import app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def test():
+    pass
