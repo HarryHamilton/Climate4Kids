8518db5b6ab11be7de72dffaa3752198e04af787
JamesNewsome02 <J.Newsome2@newcastle.ac.uk>
2021-12-04 12:00:10 +0000

    Added blueprints

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,22 @@
-from flaskr.__init__ import app
+import os
+from flask import Flask, render_template
+from users.views import users
 
+# APP CONFIGURATION
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='test'  # TODO: Configuration values here will later be put into a config.py
+)
+# BLUEPRINT REGISTRATION
+app.register_blueprint(users)
+
+
+# HOME PAGE
 @app.route("/")
-def test():
-    pass
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
